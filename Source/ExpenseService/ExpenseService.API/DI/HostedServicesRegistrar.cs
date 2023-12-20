7d5bcb1613a66603e0de72183d0a4a5cc5b01c1a
mbbarkt3sto <mbark1236@email.com>
2023-12-20 09:56:08 +0100

    Refactor (ExpenseService): Refactor HostedServicesRegistrar to simplify code

--- Source/ExpenseService/ExpenseService.API/DI/HostedServicesRegistrar.cs
+++ Source/ExpenseService/ExpenseService.API/DI/HostedServicesRegistrar.cs
@@ -7,8 +7,8 @@ public static class HostedServicesRegistrar
 	public static IServiceCollection RegisterHostedServices(this IServiceCollection services)
 	{
 		services.AddSingleton<OutboxProcessorService>();
-		services.AddHostedService<OutboxProcessorService>( provider => provider.GetService<OutboxProcessorService>()!);
-		
+		services.AddHostedService( provider => provider.GetService<OutboxProcessorService>()!);
+
 		return services;
 	}
 }
